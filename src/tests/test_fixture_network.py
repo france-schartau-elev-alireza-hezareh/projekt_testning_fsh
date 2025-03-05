d4355b0406f1c697a2ef0aaed552d5dfcef13054
Habbenur <habbenurozen@gmail.com>
2025-03-05 14:42:11 +0100

    Lagt update tester till fixture filen.

--- src/tests/test_fixture_network.py
+++ src/tests/test_fixture_network.py
@@ -30,8 +30,26 @@ def test_show_response_prefix(ncm): # Testmetod
     ncm.update_response_prefix("Standard Response") 
     assert ncm.show_response_prefix() == "response_prefix: Standard Response" 
     print("Response prefix updated to Standard Response.") 
-    
 
+def test_update_hostname(ncm):
+    ncm.update_hostname("2")
+    updated_hostname = ncm.show_hostname()
+    assert updated_hostname == "hostname: 2"
+    """Testa att uppdatera hostname-konfiguration"""
+
+def test_update_response_prefix(ncm):
+    ncm.update_response_prefix("New Response")
+    updated_response_prefix = ncm.show_response_prefix()
+    assert updated_response_prefix == "response_prefix: New Response"
+    """Testa att uppdatera response prefix-konfigurationen."""
+        
+def test_update_interface_state(ncm):
+    ncm.update_interface_state("up")
+    updated_interface_state = ncm.show_interface_state()
+    assert updated_interface_state == "interface_state: up"
+    """Testa att uppdatera interface state-konfigurationen."""
+ 
+    
 # Skriv en till testmetod som försöker skicka in ett annat värde än up eller down som värde för interface_state och verifiera att rätt exception kastas. 
 # Testet ska alltså visa grönt när exceptionet kastas, det är det som förväntas.
 def test_update_interface_state_exception(ncm): # Testmetod
--- src/tests/test_network_config.py
+++ src/tests/test_network_config.py
@@ -5,51 +5,51 @@ from network_config_manager import NetworkConfigManager
 class Test_NetworkConfigManager:
     def setup_method(self):
 
-        self.net_config_manager = NetworkConfigManager()
+        self.ncm = NetworkConfigManager()
 
-        self.net_config_manager.connect()
+        self.ncm.connect()
         """Skapar en instans av NetworkConfigManager och ansluter till enheten."""
-        self.net_config_manager.update_hostname("1")
-        self.net_config_manager.update_interface_state("down")
-        self.net_config_manager.update_response_prefix("Standard Response")
+        self.ncm.update_hostname("1")
+        self.ncm.update_interface_state("down")
+        self.ncm.update_response_prefix("Standard Response")
         """Skapar en instans av NetworkConfigManager och uppdaterar konfigurationerna."""
 
     def teardown_method(self):
-        self.net_config_manager.disconnect()
+        self.ncm.disconnect()
         """Stäng anslutningen."""
 
 # 1. Verifiera standardvärden efter setup:
     def test_show_hostname(self):
-        self.net_config_manager.show_hostname()
-        assert self.net_config_manager.show_hostname() == "hostname: 1"
+        self.ncm.show_hostname()
+        assert self.ncm.show_hostname() == "hostname: 1"
         """Testa att uppdatera hostname-konfiguration"""
  
     def test_show_response_prefix(self):
-        self.net_config_manager.show_response_prefix()
-        assert self.net_config_manager.show_response_prefix() == "response_prefix: Standard Response"
+        self.ncm.show_response_prefix()
+        assert self.ncm.show_response_prefix() == "response_prefix: Standard Response"
         """Testa att uppdatera response prefix-konfigurationen."""     
 
     def test_show_interface_state(self):
-        self.net_config_manager.show_interface_state()
-        assert self.net_config_manager.show_interface_state() == "interface_state: down"
+        self.ncm.show_interface_state()
+        assert self.ncm.show_interface_state() == "interface_state: down"
         """Testa att uppdatera interface state-konfigurationen."""
 
 #. 2. Testa uppdatering av konfiguration:
     def test_update_hostname(self):
-        self.net_config_manager.update_hostname("2")
-        updated_hostname = self.net_config_manager.show_hostname()
+        self.ncm.update_hostname("2")
+        updated_hostname = self.ncm.show_hostname()
         assert updated_hostname == "hostname: 2"
         """Testa att uppdatera hostname-konfiguration"""
 
     def test_update_response_prefix(self):
-        self.net_config_manager.update_response_prefix("New Response")
-        updated_response_prefix = self.net_config_manager.show_response_prefix()
+        self.ncm.update_response_prefix("New Response")
+        updated_response_prefix = self.ncm.show_response_prefix()
         assert updated_response_prefix == "response_prefix: New Response"
         """Testa att uppdatera response prefix-konfigurationen."""
         
     def test_update_interface_state(self):
-        self.net_config_manager.update_interface_state("up")
-        updated_interface_state = self.net_config_manager.show_interface_state()
+        self.ncm.update_interface_state("up")
+        updated_interface_state = self.ncm.show_interface_state()
         assert updated_interface_state == "interface_state: up"
         """Testa att uppdatera interface state-konfigurationen."""
  
